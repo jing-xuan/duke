07683c7d9bd894bf1083838accafb8eede017947
jing-xuan <tayjxn@gmail.com>
2019-08-23 21:35:23 +0700

    A-classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] userTasks = new String[100];
-    private static boolean[] taskDone = new boolean[100];
-    private static int numTasks = 0;
     public static void main(String[] args) {
-        Arrays.fill(taskDone, false);
         Scanner input = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,44 +11,33 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?\n");
-        String userInput;
-        do {
+        int numTasks = 0;
+        Task[] arrTask = new Task[100];
+        while (true){
+            String userInput;
             userInput = input.nextLine();
-            if (!userInput.equals("list") && !userInput.equals("bye") && !userInput.startsWith("done ")){
-                addTask(userInput);
-            } else if (userInput.equals("list")){
-                printTasks();
-            } else if (userInput.startsWith("done ")){
-                doneTask(userInput);
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. hope to see you again!");
+                break;
+            } else {
+                if (userInput.startsWith("done")){
+                    int i = userInput.charAt((userInput.length()) - 1) - '0';
+                    if (i > 0 && i <= numTasks) {
+                        arrTask[i - 1].markAsDone();
+                        System.out.println("Marked as done:\n[" + arrTask[i - 1].getStatusIcon() + "] " + arrTask[i - 1].description);
+                    } else {
+                        System.out.println("No such task");
+                    }
+                } else if (userInput.startsWith("list")){
+                    for (int i = 0; i < numTasks; i++){
+                        System.out.println((i + 1) + ". [" + arrTask[i].getStatusIcon() + "] " + arrTask[i].description);
+                    }
+                } else {
+                    arrTask[numTasks] = new Task(userInput);
+                    numTasks++;
+                    System.out.println("Added new task: " + userInput);
+                }
             }
-        } while(!userInput.equals("bye"));
-        System.out.println("Bye, hope to see you again!");
-    }
-
-    public static void addTask(String task){
-        userTasks[numTasks] = task;
-        System.out.println("added: " + task);
-        numTasks++;
-    }
-
-    public static void printTasks(){
-        if (numTasks == 0){
-            System.out.println("You have no tasks!");
-            return;
         }
-        for (int i = 0; i < numTasks; i++){
-            String symbol = (taskDone[i] ? "\u2713" : "\u2718");
-            System.out.println((i + 1) + ". [" + symbol + "] " + userTasks[i]);
-        }
-    }
-
-    public static void doneTask(String userInput) {
-        int i = (userInput.charAt(userInput.length() - 1)) - '0' - 1;
-        if (i < 0 || i > 99 || i > numTasks - 1) {
-            System.out.println("That is not a valid task ID");
-            return;
-        }
-        taskDone[i] = true;
-        System.out.println("Have marked this task as done:\n [\u2713] " + userTasks[i]);
     }
 }
